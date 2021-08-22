7428aefc417135e7eb7b4d6edd6381f1d5bdaac5
rensuxia <18861851854@163.com>
2021-08-22 21:20:51 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'kelly',
+    age: 18,
+    movies: [
+      {id:1, name: 'test'},
+      {id:2, name: 'why'},
+      {id:3, name: 'ceshi'},
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    console.log('click')
+    // 错误的做法，界面不刷新
+    // this.data.counter +=1
+    // 2、this.setData()
+    this.setData({
+      counter: this.data.counter += 1
+    })
+    // console.log(this.data.counter)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnClickSub() {
+    // console.log('剑豪')
+    this.setData({
+      counter: this.data.counter -= 1
+    })
   }
 })
